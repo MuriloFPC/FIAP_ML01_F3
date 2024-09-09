056b73e7f93d6a5d6ea2ab02c1706eb06577f697
MuriloFPC <murilofpc@gmail.com>
2024-09-08 23:37:05 -0300

    Commit

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, send_file
 
+import pickle
 app = Flask(__name__)
 
+app.model_nota_ch = None
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    if request.method == 'POST' or request.method == 'GET':
+        if app.model_nota_ch is None:
+            app.model_nota_ch = import_model('nota_ch')
+    return render_template('index.html')
+
+
+def import_model(model_name):
+    with open(f'./models_pickle/model_{model_name}.pkl', 'rb') as f:
+        model = pickle.load(f)
+    return model
 
 
 if __name__ == '__main__':
