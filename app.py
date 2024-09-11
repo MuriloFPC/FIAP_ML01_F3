d05b6ae16ea5b6c9636d7f4c9ef2fde9c1f6d4be
MuriloFPC <murilofpc@gmail.com>
2024-09-10 23:06:52 -0300

    Commit

--- app.py
+++ app.py
@@ -1,24 +1,117 @@
 from flask import Flask, render_template, request, send_file
-
+import pandas as pd
 import pickle
+
 app = Flask(__name__)
 
 app.model_nota_ch = None
+app.model_nota_cn = None
+app.model_nota_lc = None
+app.model_nota_mt = None
+app.model_nota_re = None
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def hello_world():  # put application's code here
-    if request.method == 'POST' or request.method == 'GET':
-        if app.model_nota_ch is None:
-            app.model_nota_ch = import_model('nota_ch')
     return render_template('index.html')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+
+        errors = validate_form(request.form)
+        if errors is not None:
+            return errors
+
+        if app.model_nota_ch is None:
+            app.model_nota_ch = import_model('nota_ch')
+            pass
+
+        if app.model_nota_cn is None:
+            #app.model_nota_cn = import_model('nota_cn')
+            pass
+
+        if app.model_nota_lc is None:
+            #app.model_nota_lc = import_model('nota_lc')
+            pass
+
+        if app.model_nota_mt is None:
+            #app.model_nota_mt = import_model('nota_mt')
+            pass
+
+        if app.model_nota_re is None:
+            #app.model_nota_re = import_model('nota_redacao')
+            pass
+
+        df =  pd.DataFrame([request.form.to_dict()])
+        #y_ch = app.model_nota_ch.predict(df)
+        #y_cn = app.model_nota_cn.predict(df)
+        #y_lc = app.model_nota_lc.predict(df)
+        #y_mt = app.model_nota_mt.predict(df)
+        #y_re = app.model_nota_re.predict(df)
+        y_ch = 458.3378
+        response = f"""
+        <div class="alert alert-success">
+            <h4>Nota ENEM prevista</h4>
+            <p><strong>CH:</strong>{round(y_ch,2)}</p>
+            <p><strong>CN:</strong>{round(y_ch,2)}</p>
+            <p><strong>LC:</strong>{round(y_ch,2)} </p>
+            <p><strong>MT:</strong>{round(y_ch,2)} </p>
+            <p><strong>Redação:</strong>{round(y_ch,2)} </p>
+        </div>
+        """
+        return response
 
 def import_model(model_name):
-    with open(f'./models_pickle/model_{model_name}.pkl', 'rb') as f:
+    with open(f'./models_pickle/model_XGBoost_{model_name}.pkl', 'rb') as f:
         model = pickle.load(f)
     return model
+def validate_form(form):
+    list_errors = []
+    form_keys = form.keys()
 
+    dict_perguntas = {
+        'Q001':1,
+        'Q002':2,
+        'Q003':3,
+        'Q004':4,
+        'Q005':5,
+        'Q006':6,
+        'Q007':7,
+        'Q008':8,
+        'Q009':9,
+        'Q010':10,
+        'Q011':11,
+        'Q012':12,
+        'Q013':13,
+        'Q014':14,
+        'Q015':15,
+        'Q016':16,
+        'Q017':17,
+        'Q018':18,
+        'Q019':19,
+        'Q020':20,
+        'Q021':21,
+        'Q022':22,
+        'Q023':23,
+        'Q024':24,
+        'Q025':25
+    }
+
+    for key in dict_perguntas.keys():
+        if not form_keys.__contains__(key):
+            list_errors.append(f'<p><strong>Pergunta {dict_perguntas[key]} sem resposta</strong></p>')
+
+    if list_errors is not []:
+        response = f"""
+                <div class="alert alert-danger">
+                    <h4>Erro no preenchimento do Form</h4>
+                    {''.join(list_errors)}
+                </div>
+                """
+        return response
+
+    return None
 
 if __name__ == '__main__':
     app.run()
--- models_training/main.py
+++ models_training/main.py
@@ -6,6 +6,8 @@ import pickle
 import logging
 import numpy as np
 from sklearn import metrics
+from xgboost import XGBRegressor
+
 
 def train_model_RandomForestRegressor(X, Y,filename):
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
@@ -16,90 +18,115 @@ def train_model_RandomForestRegressor(X, Y,filename):
     logger.info(f'Mean Absolute Error (MAE): {metrics.mean_absolute_error(y_test, preds_valid)}')
     logger.info(f'Mean Squared Error (MSE): {metrics.mean_squared_error(y_test, preds_valid)}')
     logger.info(f'Root Mean Squared Error (RMSE): {np.sqrt(metrics.mean_squared_error(y_test, preds_valid))}')
-    mape = np.mean(np.abs((y_test - preds_valid) / np.abs(y_test)))
-    logger.info(f'Mean Absolute Percentage Error (MAPE):{ round(mape * 100, 2)}')
-    logger.info(f'Accuracy: {round(100 * (1 - mape), 2)}')
+    mape = metrics.mean_absolute_percentage_error(y_test,preds_valid)
+    logger.info(f'Mean Absolute Percentage Error (MAPE):{mape}')
+    logger.info(f'Accuracy: {1 - mape}')
     with open(f'../models_pickle/model_{filename}.pkl', 'wb') as f:
         pickle.dump(model, f)
 
+def train_model_XGBoost(X,Y,filename):
+    params = {
+        'n_estimators': 100,
+        'max_depth': 25,
+        'learning_rate': 0.1,
+    }
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    model = XGBRegressor(**params)
+    model.fit(X_train, y_train)
+    preds_valid = model.predict(X_test)
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='app.log')
-logger = logging.getLogger('train_model')
+    # log first five predictions
+    logger.info(f'Predictions: {preds_valid[:5]}')
+    # log first five test values
+    logger.info(f'Real: {y_test[:5]}')
+    logger.info(f'Mean Absolute Error (MAE): {metrics.mean_absolute_error(y_test, preds_valid)}')
+    logger.info(f'Mean Squared Error (MSE): {metrics.mean_squared_error(y_test, preds_valid)}')
+    logger.info(f'Root Mean Squared Error (RMSE): {np.sqrt(metrics.mean_squared_error(y_test, preds_valid))}')
+    mape = metrics.mean_absolute_percentage_error(y_test, preds_valid)
+    logger.info(f'Mean Absolute Percentage Error (MAPE):{mape}')
+    logger.info(f'Accuracy: {1 - mape}')
+    with open(f'../models_pickle/model_XGBoost_{filename}.pkl', 'wb') as f:
+        pickle.dump(model, f)
 
+if __name__ == '__main__':
 
-
-# Load data
-logger.info('Loading data...')
-csv = pd.read_csv('../csv/Completo.csv', sep=';', encoding='latin-1')
-logger.info('Data loaded')
-logger.info('Preprocessing data...')
-csv.dropna(inplace=True)
-logger.info('Data preprocessed')
-
-colunas_remover = [x for x in csv.columns[0:51] if x not in ['TP_ESCOLA']]
-
-X = csv.drop(columns=colunas_remover)
-y_nota_ch = csv['NU_NOTA_CH'] #predict column
-y_nota_cn = csv['NU_NOTA_CN'] #predict column
-y_nota_lc = csv['NU_NOTA_LC'] #predict column
-y_nota_mt = csv['NU_NOTA_MT'] #predict column
-y_nota_redacao = csv['NU_NOTA_REDACAO'] #predict column
-
-
-mymap = {'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6,'G':7,'H':8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P': 16, 'Q':17}
-
-logger.info('Ajustandando arquivo')
-X = X.map(lambda s: mymap.get(s) if s in mymap else s)
-logger.info('Arquivo ajustado')
-
-logger.info('Training models...')
-# Train models
-logger.info('Training model nota_ch...')
-start_time = datetime.now()
-train_model_RandomForestRegressor(X, y_nota_ch, 'nota_ch')
-end_time = datetime.now()
-execution_time = end_time - start_time
-formatted_time = str(execution_time).split('.')[0]
-logger.info(f"Execution time: {formatted_time}")
-logger.info('Model nota_ch trained')
-
-logger.info('Training model nota_cn...')
-start_time = datetime.now()
-train_model_RandomForestRegressor(X, y_nota_cn, 'nota_cn')
-end_time = datetime.now()
-execution_time = end_time - start_time
-formatted_time = str(execution_time).split('.')[0]
-logger.info(f"Execution time: {formatted_time}")
-logger.info('Model nota_cn trained')
-
-logger.info('Training model nota_lc...')
-start_time = datetime.now()
-train_model_RandomForestRegressor(X, y_nota_lc, 'nota_lc')
-end_time = datetime.now()
-execution_time = end_time - start_time
-formatted_time = str(execution_time).split('.')[0]
-logger.info(f"Execution time: {formatted_time}")
-logger.info('Model nota_lc trained')
-
-logger.info('Training model nota_mt...')
-start_time = datetime.now()
-train_model_RandomForestRegressor(X, y_nota_mt, 'nota_mt')
-end_time = datetime.now()
-execution_time = end_time - start_time
-formatted_time = str(execution_time).split('.')[0]
-logger.info(f"Execution time: {formatted_time}")
-logger.info('Model nota_mt trained')
-
-logger.info('Training model nota_redacao...')
-start_time = datetime.now()
-train_model_RandomForestRegressor(X, y_nota_redacao, 'nota_redacao')
-end_time = datetime.now()
-execution_time = end_time - start_time
-formatted_time = str(execution_time).split('.')[0]
-logger.info(f"Execution time: {formatted_time}")
-logger.info('Model nota_mt nota_redacao')
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='app.log')
+    logger = logging.getLogger('train_model')
 
 
-logger.info('Trained models')
+
+    # Load data
+    logger.info('Loading data...')
+    csv = pd.read_csv('../csv/Completo.csv', sep=';', encoding='latin-1')
+    logger.info('Data loaded')
+    logger.info('Preprocessing data...')
+    csv.dropna(inplace=True)
+    logger.info('Data preprocessed')
+
+    colunas_remover = [x for x in csv.columns[0:51] if x not in ['TP_ESCOLA','TP_FAIXA_ETARIA','TP_ESCOLA','TP_ENSINO']]
+
+    X = csv.drop(columns=colunas_remover)
+    y_nota_ch = csv['NU_NOTA_CH'] #predict column
+    y_nota_cn = csv['NU_NOTA_CN'] #predict column
+    y_nota_lc = csv['NU_NOTA_LC'] #predict column
+    y_nota_mt = csv['NU_NOTA_MT'] #predict column
+    y_nota_redacao = csv['NU_NOTA_REDACAO'] #predict column
+
+
+    mymap = {'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6,'G':7,'H':8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P': 16, 'Q':17}
+
+    logger.info('Ajustandando arquivo')
+    X = X.map(lambda s: mymap.get(s) if s in mymap else s)
+    logger.info('Arquivo ajustado')
+
+    logger.info('Training models...')
+    # Train models
+    logger.info('Training model nota_ch...')
+    start_time = datetime.now()
+    train_model_XGBoost(X, y_nota_ch, 'nota_ch')
+    end_time = datetime.now()
+    execution_time = end_time - start_time
+    formatted_time = str(execution_time).split('.')[0]
+    logger.info(f"Execution time: {formatted_time}")
+    logger.info('Model nota_ch trained')
+
+    logger.info('Training model nota_cn...')
+    start_time = datetime.now()
+    train_model_XGBoost(X, y_nota_cn, 'nota_cn')
+    end_time = datetime.now()
+    execution_time = end_time - start_time
+    formatted_time = str(execution_time).split('.')[0]
+    logger.info(f"Execution time: {formatted_time}")
+    logger.info('Model nota_cn trained')
+
+    logger.info('Training model nota_lc...')
+    start_time = datetime.now()
+    train_model_XGBoost(X, y_nota_lc, 'nota_lc')
+    end_time = datetime.now()
+    execution_time = end_time - start_time
+    formatted_time = str(execution_time).split('.')[0]
+    logger.info(f"Execution time: {formatted_time}")
+    logger.info('Model nota_lc trained')
+
+    logger.info('Training model nota_mt...')
+    start_time = datetime.now()
+    train_model_XGBoost(X, y_nota_mt, 'nota_mt')
+    end_time = datetime.now()
+    execution_time = end_time - start_time
+    formatted_time = str(execution_time).split('.')[0]
+    logger.info(f"Execution time: {formatted_time}")
+    logger.info('Model nota_mt trained')
+
+    logger.info('Training model nota_redacao...')
+    start_time = datetime.now()
+    train_model_XGBoost(X, y_nota_redacao, 'nota_redacao')
+    end_time = datetime.now()
+    execution_time = end_time - start_time
+    formatted_time = str(execution_time).split('.')[0]
+    logger.info(f"Execution time: {formatted_time}")
+    logger.info('Model nota_mt nota_redacao')
+
+
+    logger.info('Trained models')
 
 
